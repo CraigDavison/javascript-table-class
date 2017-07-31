cb6472a3c01bc7ea863f56418d8f8fefcce37793
Craig <CraigDavison@users.noreply.github.com>
2017-07-31 10:47:39 +0100

    Add files via upload
    This is an early build stage which includes a json array for testing.

---
+++ js_table_generator.php
@@ -0,0 +1,386 @@
+<?php
+include 'html_head.php';
+$json = '[
+  {
+    "_id": "597cfa2dec0fb3f962f75c6e",
+    "index": 0,
+    "guid": "1a7aedc8-6250-4555-a836-a1814e47ca2a",
+    "isActive": true,
+    "balance": "$3,133.83",
+    "picture": "http://placehold.it/32x32",
+    "age": 33,
+    "eyeColor": "green",
+    "name": "Nixon Tillman",
+    "gender": "male",
+    "company": "VIRVA",
+    "email": "nixontillman@virva.com",
+    "phone": "+1 (862) 482-3092",
+    "address": "868 Kermit Place, Hiwasse, Iowa, 7514",
+    "about": "Eu cupidatat dolore velit dolore aliqua incididunt ipsum incididunt occaecat qui fugiat. Nisi exercitation in deserunt elit. Ad voluptate nisi non fugiat consequat qui Lorem et aliquip commodo nostrud consectetur incididunt ad. Ut id laborum id elit nostrud commodo reprehenderit officia. Voluptate aliquip sit consequat sunt adipisicing voluptate mollit aute magna. Excepteur et est laboris anim. Laboris excepteur cupidatat excepteur excepteur eiusmod exercitation veniam eiusmod dolore dolor cupidatat id ex.\r\n",
+    "registered": "2016-02-10T04:22:28 -00:00",
+    "latitude": 78.41944,
+    "longitude": -89.709154,
+    "tags": [
+      "cupidatat",
+      "irure",
+      "laborum",
+      "veniam",
+      "dolore",
+      "officia",
+      "enim"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Bell Ortiz"
+      },
+      {
+        "id": 1,
+        "name": "Jill Kane"
+      },
+      {
+        "id": 2,
+        "name": "Bradley Hartman"
+      }
+    ],
+    "greeting": "Hello, Nixon Tillman! You have 1 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "597cfa2dea0ff24ee5804720",
+    "index": 1,
+    "guid": "6a2f70e9-9da1-4127-9f59-a5d758538206",
+    "isActive": true,
+    "balance": "$3,305.17",
+    "picture": "http://placehold.it/32x32",
+    "age": 33,
+    "eyeColor": "blue",
+    "name": "Cooley Petty",
+    "gender": "male",
+    "company": "ZOXY",
+    "email": "cooleypetty@zoxy.com",
+    "phone": "+1 (907) 564-2964",
+    "address": "782 Lamont Court, Reno, Montana, 6064",
+    "about": "Mollit enim do fugiat in cupidatat. Ut cupidatat consectetur in non proident pariatur sunt in est adipisicing. Officia dolor irure esse veniam sunt culpa aute aute magna non. Sint sint pariatur Lorem excepteur excepteur aute sint reprehenderit sit eu. Laborum fugiat quis duis occaecat sint ipsum id in irure sint voluptate.\r\n",
+    "registered": "2014-11-12T09:21:52 -00:00",
+    "latitude": -6.681485,
+    "longitude": 69.313413,
+    "tags": [
+      "quis",
+      "laborum",
+      "laboris",
+      "consequat",
+      "irure",
+      "aliquip",
+      "incididunt"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Beck Reid"
+      },
+      {
+        "id": 1,
+        "name": "Stephens Kelly"
+      },
+      {
+        "id": 2,
+        "name": "Sherri Webb"
+      }
+    ],
+    "greeting": "Hello, Cooley Petty! You have 10 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "597cfa2de4eb8a7d5c309881",
+    "index": 2,
+    "guid": "000bb031-83a7-4066-92eb-90afb3c0d5d0",
+    "isActive": false,
+    "balance": "$1,165.47",
+    "picture": "http://placehold.it/32x32",
+    "age": 28,
+    "eyeColor": "brown",
+    "name": "Wiley Hewitt",
+    "gender": "male",
+    "company": "EZENT",
+    "email": "wileyhewitt@ezent.com",
+    "phone": "+1 (943) 464-3731",
+    "address": "886 Vandervoort Place, Sehili, Virgin Islands, 4968",
+    "about": "Amet anim anim tempor in aute sit nostrud culpa consequat aliqua. Anim ipsum eu adipisicing amet minim deserunt aute nisi officia do irure sit. Mollit enim laboris excepteur eiusmod magna excepteur tempor laboris dolore voluptate voluptate aliquip magna pariatur. Officia incididunt dolore Lorem irure aliquip ad est. Do labore aliquip ea culpa nostrud ut id Lorem ad. Aliquip ipsum nostrud ullamco sint non id ut quis laborum et do mollit eiusmod. Sit velit velit aute dolor mollit.\r\n",
+    "registered": "2014-04-26T10:01:54 -01:00",
+    "latitude": -53.623867,
+    "longitude": 59.991812,
+    "tags": [
+      "veniam",
+      "veniam",
+      "ullamco",
+      "deserunt",
+      "amet",
+      "esse",
+      "mollit"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Joyce Pierce"
+      },
+      {
+        "id": 1,
+        "name": "Fischer Bailey"
+      },
+      {
+        "id": 2,
+        "name": "Baldwin Patel"
+      }
+    ],
+    "greeting": "Hello, Wiley Hewitt! You have 8 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "597cfa2de150fe3ee4c407f1",
+    "index": 3,
+    "guid": "8a46c5dc-cb33-4dbd-beb6-f66b869ca030",
+    "isActive": true,
+    "balance": "$2,287.17",
+    "picture": "http://placehold.it/32x32",
+    "age": 30,
+    "eyeColor": "blue",
+    "name": "Sallie Fernandez",
+    "gender": "female",
+    "company": "ATGEN",
+    "email": "salliefernandez@atgen.com",
+    "phone": "+1 (841) 471-3598",
+    "address": "399 Cortelyou Road, Dahlen, Hawaii, 1680",
+    "about": "Laborum irure tempor dolore qui culpa veniam. Nostrud qui consequat non consectetur cupidatat. Do labore fugiat adipisicing sint eu non sit est irure sunt adipisicing. Exercitation est sint excepteur sint eu laborum dolore quis. Laborum nostrud incididunt commodo ad consectetur cillum. Consequat non ex et magna do aliqua eiusmod ea consectetur pariatur fugiat laboris ea.\r\n",
+    "registered": "2015-05-22T02:23:35 -01:00",
+    "latitude": 19.032344,
+    "longitude": 97.938896,
+    "tags": [
+      "eu",
+      "nostrud",
+      "sint",
+      "ipsum",
+      "qui",
+      "veniam",
+      "dolor"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Ofelia Ruiz"
+      },
+      {
+        "id": 1,
+        "name": "Jennie Kinney"
+      },
+      {
+        "id": 2,
+        "name": "Chavez Mcintosh"
+      }
+    ],
+    "greeting": "Hello, Sallie Fernandez! You have 5 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "597cfa2d39e8643bf7ce193b",
+    "index": 4,
+    "guid": "bef29f14-f43a-415e-b915-948e01607ce8",
+    "isActive": true,
+    "balance": "$2,569.51",
+    "picture": "http://placehold.it/32x32",
+    "age": 30,
+    "eyeColor": "green",
+    "name": "Rebecca Mcconnell",
+    "gender": "female",
+    "company": "OTHERWAY",
+    "email": "rebeccamcconnell@otherway.com",
+    "phone": "+1 (863) 593-3609",
+    "address": "165 Poplar Avenue, Crenshaw, West Virginia, 9877",
+    "about": "Deserunt officia qui officia laboris cupidatat ex sunt. Magna exercitation adipisicing occaecat magna cillum elit in enim nostrud. Magna consectetur qui sint cillum ipsum irure consectetur officia. Sint aliqua anim consequat deserunt exercitation aliquip commodo esse. Fugiat esse sint aute ullamco in quis eu labore aliqua nostrud nostrud sunt qui.\r\n",
+    "registered": "2017-04-10T04:05:18 -01:00",
+    "latitude": 54.221546,
+    "longitude": -35.528436,
+    "tags": [
+      "enim",
+      "consectetur",
+      "anim",
+      "aliqua",
+      "aliqua",
+      "velit",
+      "incididunt"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Black Harper"
+      },
+      {
+        "id": 1,
+        "name": "Kelly Weiss"
+      },
+      {
+        "id": 2,
+        "name": "Kane Craft"
+      }
+    ],
+    "greeting": "Hello, Rebecca Mcconnell! You have 3 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "597cfa2d001978a57aac2e77",
+    "index": 5,
+    "guid": "1f43daf7-dbb5-4a56-b84a-d038e5de9662",
+    "isActive": true,
+    "balance": "$3,913.52",
+    "picture": "http://placehold.it/32x32",
+    "age": 38,
+    "eyeColor": "brown",
+    "name": "Hodge Cooke",
+    "gender": "male",
+    "company": "CALLFLEX",
+    "email": "hodgecooke@callflex.com",
+    "phone": "+1 (968) 548-2232",
+    "address": "936 Dupont Street, Otranto, Alabama, 362",
+    "about": "Commodo id officia elit reprehenderit adipisicing laboris enim. Quis quis occaecat consequat duis minim sit aute enim est. Lorem amet eu ea veniam quis amet adipisicing velit aliqua.\r\n",
+    "registered": "2016-08-05T02:34:06 -01:00",
+    "latitude": -10.213129,
+    "longitude": 47.263696,
+    "tags": [
+      "labore",
+      "magna",
+      "laborum",
+      "labore",
+      "consectetur",
+      "Lorem",
+      "excepteur"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Dudley Hoover"
+      },
+      {
+        "id": 1,
+        "name": "Bertha Norman"
+      },
+      {
+        "id": 2,
+        "name": "Fernandez Lloyd"
+      }
+    ],
+    "greeting": "Hello, Hodge Cooke! You have 9 unread messages.",
+    "favoriteFruit": "banana"
+  }
+]';
+$json = json_encode($json);
+ ?>
+<button style="btn btn-primary" type="button" onclick='gen_table(<?php echo $json ?>,"table-container")'>Geberate Table</button>
+<div id="table-container">
+
+</div>
+ <script>
+function gen_table(json,container){
+    var table = new HTMLtable(json,container)
+    table.create_table();
+    table.add_caption('CAPTION','This Table');
+    table.create_section('THEAD');
+    table.add_row();
+    table.add_cell('th');
+    table.add_cell('th');
+    table.add_cell('th');
+    table.create_section('TBODY');
+    table.add_data()
+    table.add_htmlClass(table.table,['table','table-condensed'])
+    table.add_htmlClass(table.body,['tbody','table-body'])
+
+    console.log(table.table);
+
+    document.getElementById(container).appendChild(table.table)
+
+
+}
+class HTMLtable {
+
+    constructor(json,container){
+        this.data = json;
+        this.cont = container;
+        this.section = '';
+    }
+
+    create_table(){
+        this.table = document.createElement('TABLE');
+
+    }
+
+
+    create_section(sec,html,attr){
+        switch(sec){
+            case 'CAPTION':
+                this.section = document.createElement(sec)
+                this.section.setAttribute('class','table_section');
+                this.add_caption(this.section);
+
+                break;
+            case 'THEAD':
+                this.section = document.createElement(sec)
+                this.section.setAttribute('class','table_head');
+                this.add_head(this.section);
+                break;
+            case 'TBODY':
+                this.section = document.createElement(sec)
+                this.section.setAttribute('class','table_body');
+                this.add_body(this.section);
+                break;
+            case 'footer':
+                //code blocl
+                break;
+            default:
+                //code block
+        }
+    }
+    add_caption(sec,html,attr){
+        this.section = document.createElement(sec)
+        this.section.innerHTML = html;
+        this.section.setAttribute('class','table_section');
+        this.caption = this.section
+        this.table.appendChild(this.caption);
+    }
+    add_head(head){
+        this.head = head
+        this.table.appendChild(this.head);
+    }
+    add_body(body){
+        this.body = body
+        this.table.appendChild(this.body);
+    }
+    add_footer(){
+
+    }
+    add_row(){
+        var sec = this.section
+        this.row = document.createElement('tr')
+        sec.appendChild(this.row)
+    }
+    add_cell(cell,html){
+        this.cell = document.createElement(cell);
+        this.row.appendChild(this.cell)
+    }
+    add_htmlClass(elem, klasses){
+        klasses.forEach(function(e){
+            elem.classList.add(e)
+        })
+    }
+    add_data(){
+        let data = $.parseJSON(this.data)
+        data.forEach(function(k,v){
+            super.this.add_row();
+            k.forEach(function(k,v){
+                add_cell('td',v)
+            });
+        });
+    }
+
+}// end class table
+
+
+ </script>
